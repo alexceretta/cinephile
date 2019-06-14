70cce0a1ca35bae8bb693d0ad7e5a932c655a786
Ceretta Soares <AXSS@gft.com>
2019-06-14 13:11:05 -0300

    * Syling movie list. * Adding infinite scroll package.

--- cinephile-web/src/app/app.module.ts
+++ cinephile-web/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatGridListModule, MatCardModule } from '@angular/material';
 import { LayoutComponent } from './layout/layout.component';
 import { HomeComponent } from './home/home.component';
@@ -27,6 +28,7 @@ import { ConfigurationService } from './configuration/configuration-service';
     AppRoutingModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
+    InfiniteScrollModule,
     MatToolbarModule,
     MatButtonModule,
     MatSidenavModule,
